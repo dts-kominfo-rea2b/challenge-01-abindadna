069f4e5a4b13995c3c9a58aecb73190834df5ed1
abindadna <abindadna6397@gmail.com>
2022-05-29 19:23:06 +0700

    modified arr favoriteColor

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 // TODO: Buatlah data dari kedua user di sini
 // Tentukan tipe data dan struktur data yang menurut kalian tepat
 // Kalian bisa membuat variabel lain untuk dimasukkan ke dalam atribut user agar lebih mudah dibaca
+
 const firstUser = {
   name: "Monica",
   gender: "Female",
   age: 17,
   email: "monica@dingdong.com",
-  favoriteColor: ["Yellow, Pink, White, Purple"],
+  favoriteColor: ["Yellow", "Pink", "White", "Purple"],
   isHavePet: true,
   education: [
     {
